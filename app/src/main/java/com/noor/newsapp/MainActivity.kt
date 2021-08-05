f3ed6ad3b9ba1fe14b7f3be8f8cd52535d75c72a
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-05 21:09:48 +0300

    RecyclerView eklendi ve haber gösterimi yapildi

--- app/src/main/java/com/noor/newsapp/MainActivity.kt
+++ app/src/main/java/com/noor/newsapp/MainActivity.kt
@@ -3,13 +3,22 @@ package com.noor.newsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.noor.newsapp.databinding.ActivityMainBinding
+import com.noor.newsapp.presentation.viewModel.NewsViewModel
+import com.noor.newsapp.presentation.viewModel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: NewsViewModel
+    @Inject
+    lateinit var factory: NewsViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +30,7 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigationBar.setupWithNavController(
             navController
         )
+        viewModel = ViewModelProvider(this, factory)
+            .get(NewsViewModel::class.java)
     }
 }
--- app/src/main/java/com/noor/newsapp/NewsFragment.kt
+++ app/src/main/java/com/noor/newsapp/NewsFragment.kt
@@ -5,18 +5,74 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.load.engine.Resource
 import com.noor.newsapp.databinding.FragmentNewsBinding
+import com.noor.newsapp.presentation.adapter.NewsAdapter
+import com.noor.newsapp.presentation.viewModel.NewsViewModel
 
 class NewsFragment : Fragment() {
 
     private lateinit var binding: FragmentNewsBinding
+    private lateinit var viewModel: NewsViewModel
+    private lateinit var newsAdapter: NewsAdapter
+    private val country = "tr"
+    private val page = 1
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentNewsBinding.inflate(layoutInflater, container, false)
+        activity?.title = "Haberler"
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+        initRecyclerView()
+        viewNewsList()
+    }
+
+    private fun viewNewsList() {
+        viewModel.getNewsHeadLines(country, page)
+        viewModel.newsHeadLines.observe(viewLifecycleOwner, { response ->
+            when(response){
+                is com.noor.newsapp.data.util.Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let {
+                        newsAdapter.differ.submitList(it.articles.toList())
+                    }
+                }
+                is com.noor.newsapp.data.util.Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let {
+                        Toast.makeText(activity, "An error occurred: $it", Toast.LENGTH_LONG).show()
+                    }
+                }
+                is com.noor.newsapp.data.util.Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun initRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun showProgressBar(){
+        binding.progressBar.visibility = View.VISIBLE
+    }
+    private fun hideProgressBar(){
+        binding.progressBar.visibility = View.INVISIBLE
     }
 
 }
